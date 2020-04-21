5b346df9d109f45e494b3f1c69ccdd793e3a26ed
sh0rez <me@shorez.de>
2020-04-21 14:47:05 +0200

    test: remove flake

--- cmd/jb/install_test.go
+++ cmd/jb/install_test.go
@@ -72,9 +72,9 @@ func testInstallCommandWithJsonnetHome(t *testing.T, jsonnetHome string) {
 		},
 		{
 			Name:                    "single",
-			URIs:                    []string{"github.com/grafana/loki/production/ksonnet/loki"},
+			URIs:                    []string{"github.com/grafana/loki/production/ksonnet/loki@bd4d516262c107a0bde7a962fa2b1e567a2c21e5"},
 			ExpectedCode:            0,
-			ExpectedJsonnetFile:     []byte(`{"version":1,"dependencies":[{"source":{"git":{"remote":"https://github.com/grafana/loki","subdir":"production/ksonnet/loki"}},"version":"master","single":true}],"legacyImports":true}`),
+			ExpectedJsonnetFile:     []byte(`{"version":1,"dependencies":[{"source":{"git":{"remote":"https://github.com/grafana/loki","subdir":"production/ksonnet/loki"}},"version":"bd4d516262c107a0bde7a962fa2b1e567a2c21e5","single":true}],"legacyImports":true}`),
 			ExpectedJsonnetLockFile: []byte(`{"version":1,"dependencies":[{"source":{"git":{"remote":"https://github.com/grafana/loki","subdir":"production/ksonnet/loki"}},"version":"bd4d516262c107a0bde7a962fa2b1e567a2c21e5","sum":"ExovUKXmZ4KwJAv/q8ZwNW9BdIZlrxmoGrne7aR64wo=","single":true}],"legacyImports":false}`),
 			single:                  true,
 		},
